40b84fd5be13542881ff316c8994482b744d92b7
Angel Espinosa david <angelespid@gmail.com>
2024-11-30 19:06:31 -0500

    Laberinto y jugador....
    Signed-off-by: Angel Espinosa david <angelespid@gmail.com>

---
+++ build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Pacman_game_FILE G:/Mi unidad/UdeA/2024-2/Informatica/Talleres/Pacman_game/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/Pacman_game.exe)
+set(__QT_DEPLOY_TARGET_Pacman_game_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Pacman_game_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
